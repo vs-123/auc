566f7cc148df681eba94f5a041cfd6ea48e418e4
VS-123 <>
2026-03-10 10:48:58 +0000

    auc: switch to C11
    - switch to C11
    - use stdatomics for atomic variables

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <ctype.h>
 #include <pthread.h>
+#include <stdatomic.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,25 +12,26 @@ typedef struct
 {
    uint16_t mouse_btn; /* uiohook btn */
    unsigned int interval;
+   atomic_int mouse_x;
+   atomic_int mouse_y;
+   atomic_bool is_clicking;
+   atomic_bool is_running;
 } auc_t;
 
-volatile int g_mouse_x      = 0;
-volatile int g_mouse_y      = 0;
-volatile bool g_is_clicking = false;
-volatile bool g_is_running  = true;
+static auc_t *g_ctx = NULL;
 
 void *
 clicker_thread (void *arg)
 {
    auc_t *cfg = (auc_t *)arg;
-   while (g_is_running)
+   while (cfg->is_running)
       {
-         if (g_is_clicking)
+         if (cfg->is_clicking)
             {
                uiohook_event event     = { .type = EVENT_MOUSE_PRESSED };
                event.data.mouse.button = (uint16_t)cfg->mouse_btn;
-               event.data.mouse.x      = (int16_t)g_mouse_x;
-               event.data.mouse.y      = (int16_t)g_mouse_y;
+               event.data.mouse.x      = (int16_t)cfg->mouse_x;
+               event.data.mouse.y      = (int16_t)cfg->mouse_y;
 
                hook_post_event (&event);
                event.type = EVENT_MOUSE_RELEASED;
@@ -48,25 +50,31 @@ clicker_thread (void *arg)
 void
 listener (uiohook_event *const event)
 {
+   if (!g_ctx)
+      {
+         return;
+      }
+
    switch (event->type)
       {
       case EVENT_MOUSE_MOVED:
       case EVENT_MOUSE_DRAGGED:
-         g_mouse_x = event->data.mouse.x;
-         g_mouse_y = event->data.mouse.y;
+         atomic_store (&g_ctx->mouse_x, event->data.mouse.x);
+         atomic_store (&g_ctx->mouse_y, event->data.mouse.y);
          break;
       case EVENT_KEY_PRESSED:
          if (event->data.keyboard.keycode == VC_F6)
             {
-               g_is_clicking = !g_is_clicking;
+               bool is_clicking = atomic_load (&g_ctx->is_clicking);
+               atomic_store (&g_ctx->is_clicking, !is_clicking);
                printf ("[STATUS] %s\n",
-                       (g_is_clicking) ? "CLICKING NOW" : "STOPPED CLICKING");
+                       (is_clicking) ? "CLICKING NOW" : "STOPPED CLICKING");
                fflush (stdout);
             }
          if (event->data.keyboard.keycode == VC_ESCAPE)
             {
                printf ("[STATUS] EXITING...\n");
-               g_is_running = false;
+               atomic_store (&g_ctx->is_running, false);
                hook_stop ();
             }
          break;
@@ -79,11 +87,12 @@ listener (uiohook_event *const event)
 void
 run_auc (auc_t *auc)
 {
+   printf ("[STATUS] AUC STARTED, PRESS F6 TO TOGGLE\n");
    pthread_t thread_id;
    pthread_create (&thread_id, NULL, clicker_thread, auc);
    hook_set_dispatch_proc (listener);
    hook_run ();
-   g_is_running = false;
+   atomic_store (&g_ctx->is_running, false);
    pthread_join (thread_id, NULL);
 }
 
@@ -218,7 +227,7 @@ parse_arguments (int argc, char **argv, auc_t *settings)
                               interval);
                      interval = 100;
                   }
-	       settings->interval = interval;
+               settings->interval = interval;
             }
       }
 
@@ -227,6 +236,7 @@ parse_arguments (int argc, char **argv, auc_t *settings)
          fprintf (stderr, "[ERROR] MOUSE BUTTON NOT SPECIFIED, USE --HELP\n");
          return -1;
       }
+
    if (settings->interval < 100)
       {
          fprintf (stderr, "[ERROR] INTERVAL WAS NOT PROVIDED, USE --HELP\n");
@@ -240,15 +250,21 @@ int
 main (int argc, char **argv)
 {
    auc_t settings = {
-      .mouse_btn = 0,
-      .interval  = 0,
+      .mouse_btn   = 0,
+      .interval    = 0,
+      .is_clicking = false,
+      .is_running  = true,
+      .mouse_x     = 0,
+      .mouse_y     = 0,
    };
-   
+   g_ctx = &settings;
+
    int result = parse_arguments (argc, argv, &settings);
    if (result <= 0)
       {
          return (result == 0) ? 0 : 1;
       }
+
    run_auc (&settings);
    return 0;
 }
